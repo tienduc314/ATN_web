60fe2b55a526e1aa95731ce6f097b9b12ce99533
tienduc314 <91647974+tienduc314@users.noreply.github.com>
2021-11-21 16:14:03 +0700

    sd

--- conection.php
+++ conection.php
@@ -1,4 +1,4 @@
 <?php
-$conn = pg_connect("postgres://gdepmunemtksgf:8e5dcc60a520192f49a29ed114be58f6795b37ad2c962d9c5bee0feb9eb0885b@ec2-44-197-136-81.compute-1.amazonaws.com:5432/d4rlcsem44akip")
+$conn = pg_connect("postgres://bdzkbmcxkclqal:c259a5bbf2b12cecb8f26cb4e254c90bcecc913df87f4bdde5867e900751770c@ec2-44-193-182-0.compute-1.amazonaws.com:5432/d7473d30qr8ms")
 or die ("can not connect database".pg_connect_error());
 ?>
